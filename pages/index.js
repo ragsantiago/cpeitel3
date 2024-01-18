deca981a55940409102f5074a0f120f78f224576
Jovellmiranda <jcqmiranda.student@ua.edu.ph>
2024-01-18 11:39:47 +0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,34 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>My name is Jovell Christian Q. Miranda. I am 21 years old.While  I don’t have a favorite or specific music, sometimes I prefer listening to OPM music because it makes me calm when I’m really stressed about doing something. Favorite Food: Much like seafood, Hobbies are more on playing games, watching movies and looking at things that I can have an interest in. 
+        <p> My name is Jovell Christian Q. Miranda. I am 21 years old.While  I don’t have a favorite or specific music, sometimes I prefer listening to OPM music because it makes me calm when I’m really stressed about doing something. Favorite Food: Much like seafood, Hobbies are more on playing games, watching movies and looking at things that I can have an interest in. 
 IT is my first choice. My parents supported me in choosing IT.
 I don’t know what I would be expecting in my future because time changes and anything can happen. I can still continue pursuing my IT more on databases.
 This subject is new to me because it is my first time taking the subject and some new Programming Languages. 
 </p>
+
+<p>Expectations on this subject: </p>
+<ul>
+          <li>I expect the subject will interesting and useful in the future</li>
+          <li>I expect the subject will might be hard but fun</li>
+          <li>I expect the subject that will help us create useful and efficient projects on the future. </li>
+        </ul>
+
+
+
+<p>10 Things that require Zero talent</p>
+        <ol>
+          <li>Being On Time</li>
+          <li>Making An Effort</li>
+          <li>Being High Energy</li>
+          <li>Having A Positive Attitude</li>
+          <li>Being Passionate</li>
+          <li>Using Good Body Language</li>
+          <li>Being Coachable</li>
+          <li>Doing A Little Extra</li>
+          <li>Being Prepared</li>
+          <li>Having A Strong Work Ethic</li>
+        </ol>
       </section>
     </Layout>
   );
